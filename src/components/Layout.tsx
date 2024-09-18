3999cf2834e284e5b88233e53c5f1ec49ebb0ff7
teukurian <teukurian090598@gmail.com>
2024-09-18 10:57:19 +0800

    Add wisteria to body

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,12 @@
 import '../assets/css/Layout.css'
 import Navbar from './Navbar';
+import Wisteria from '../assets/images/wisteria-2.png'
 
 const Layout = () => {
     return (
     <div className="layout-container">
         <div className="layout-area">
-            
+            <img src={Wisteria} />
         </div>
         <Navbar/>
     </div>
