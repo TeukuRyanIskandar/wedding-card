cb2aa9c98a43c14b354c2a0990b232149ae01bec
teukurian <teukurian090598@gmail.com>
2024-09-19 21:35:04 +0800

    navbar alongside popup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,22 @@
 import '../assets/css/Navbar.css';
 import { NAVBAR_DATA } from '../assets/data/NAVBAR_DATA';
+import Popup from './Popup';
 
 const Navbar = () => {
     return (
-        <div className="navbar-container">
-            <div className="navbar-items">
-                {NAVBAR_DATA.map((navbarItem, index) => (
-                    <button key={index} className="navbar-button">
-                        {navbarItem.logo}
-                        <p>{navbarItem.title}</p>
-                    </button>
-                ))}
+        <>
+            <Popup />
+            <div className="navbar-container">
+                <div className="navbar-items">
+                    {NAVBAR_DATA.map((navbarItem, index) => (
+                        <button key={index} className="navbar-button">
+                            {navbarItem.logo}
+                            <p>{navbarItem.title}</p>
+                        </button>
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
