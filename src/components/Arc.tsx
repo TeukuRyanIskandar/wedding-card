bd093fa149d782efead6fa4847d1b43d17ca74c6
teukurian <teukurian090598@gmail.com>
2024-09-19 08:40:10 +0800

    Make arc a component

---
+++ src/components/Arc.tsx
@@ -0,0 +1,12 @@
+import Wisteria from '../assets/images/wisteria-2.png'
+import '../assets/css/Arc.css'
+
+const Arc = () => {
+    return (
+        <div className="arc">
+            <img src={Wisteria} />
+        </div>
+    )
+}
+
+export default Arc;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,17 @@
 import '../assets/css/Layout.css'
 import Navbar from './Navbar';
-import Wisteria from '../assets/images/wisteria-2.png'
+import Monogram from './Monogram';
+import Arc from './Arc';
 
 const Layout = () => {
     return (
-    <div className="layout-container">
-        <div className="layout-area">
-            <img src={Wisteria} />
+        <div className="layout-container">
+            <div className="layout-area">
+                <Arc />
+                <Monogram />
+                <Navbar />
+            </div>
         </div>
-        <Navbar/>
-    </div>
     )
 }
 
